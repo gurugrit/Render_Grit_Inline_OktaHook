a41ea84f6e10ab4d8dabc0c08d7a71a955bae8d6
gurugrit <guru2sail@gmail.com>
2026-03-11 14:34:44 -0700

    Update forRender_Grit_Server.js

--- forRender_Grit_Server.js
+++ forRender_Grit_Server.js
@@ -7,19 +7,44 @@ app.use(express.json());
 app.get("/GurusTokenHook", (req, res) => {
   res.send("GurusTokenHook is up");
 });
+const patients = [
+  {
+    username: 'dotttest.jon@yopmail.com',
+    ExternalServicePatientID: '1235',
+  },
+  {
+    username: 'gudorais@cisco.com',
+     ExternalServicePatientID: '6789',
+  },
+ ]
+//Token Inline Hook POST from Okta (endpoint: tokenHook)
 
-// Okta token inline hook endpoint
-app.post("/GurusTokenHook", (req, res) => {
-  console.log("Received hook payload:");
-  console.log(JSON.stringify(req.body, null, 2));
+app.post("/GurusTokenHook", (request, response) => {
+  console.log(" ");
+  console.log(request.body.data.identity.claims["preferred_username"]);
+  var patientName = request.body.data.identity.claims["preferred_username"];
+  if (patients.some(user => user.username == patientName)){
+    const arrayPosition = patients.findIndex(user => user.username == patientName);
+    const patientID = patients[arrayPosition].ExternalServicePatientID;
+    var returnValue = { "commands":[
+                          { "type":"com.okta.identity.patch",
+                            "value": [
+                                {
+                                  "op": "add",
+                                  "path": "/claims/extPatientId",
+                                  "value": patientID
+                                }
+                                ]
 
-  // minimal valid response
-  res.json({
-    commands: []
-  });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+                          }
+                                  ],
+                    }
+  console.log("Added claim to ID Token, with a value of: " + returnValue.commands[0].value[0]["value"]);
+  response.send(JSON.stringify(returnValue));
+  }
+  else {
+  console.log("Not part of patient data store");
+  response.status(204).send();
+  }
+}
+);
